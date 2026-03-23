fb3a0281567cb92353b1bd0f788f0d910fae57be
July <july@student.42.fr>
2026-03-24 02:46:27 +0800

    Fix Error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,9 +4,3 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
-```
-
-And add to your `.env.local` file (create it if it doesn't exist, in the project root):
-```
-NEXT_PUBLIC_SUPABASE_URL=https://bouzdjjpliizwwwxsgrc.supabase.co
-NEXT_PUBLIC_SUPABASE_ANON_KEY=sb_publishable_O2WwTTRcy5YG-TAJ8MjqSw_EoHpK94K
